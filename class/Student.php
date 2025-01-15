9eceef505d86aba1777909ce8c21ff65df9d6417
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-01-15 22:37:43 +0100

    add class Student

---
+++ class/Student.php
@@ -0,0 +1,18 @@
+<?php 
+require_once 'User.php';
+class student extends User{
+    public function __construct($user_id ,$first_name, $last_name , $email , $role,$password,  $profile_image , $status){
+     parent :: __construct($user_id ,$first_name, $last_name , $email , $role,$password,  $profile_image , $status);
+    }
+   public static function login($email,$password){
+     $user = self::findByEmail($email);
+        // var_dump($user->getPassword());
+        // var_dump($password);
+        if (!$user || !password_verify($password,  $user->getPassword())) {
+            throw new Exception("Invalid email or password");
+        }
+
+        return $user; 
+   }
+}
+?>
