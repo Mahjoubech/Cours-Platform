ee54bd18062753579572cf6d8f3da93ec404545d
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-01-21 15:39:47 +0100

    add some chnages

--- pages/course-preview.php
+++ pages/course-preview.php
@@ -45,14 +45,14 @@ if (!$courseDetails) {
                 <div class="flex justify-between items-center text-sm">
                     <div class="flex items-center space-x-6">
                         <span class="flex items-center">
-                            <i class="ri-phone-line mr-2"></i> +212 772508881
+                            <i class="ri-phone-line mr-2"></i> +212 684153876
                         </span>
                         <span class="flex items-center">
-                            <i class="ri-mail-line mr-2"></i> contact@youdemy.com
+                            <i class="ri-mail-line mr-2"></i> contact@taalim.com
                         </span>
                     </div>
                     <span class="flex items-center">
-                        <i class="ri-map-pin-line mr-2"></i> Massira N641 Safi, Morocco
+                        <i class="ri-map-pin-line mr-2"></i> Ennehda N641 Chichaoua, Morocco
                     </span>
                 </div>
             </div>
