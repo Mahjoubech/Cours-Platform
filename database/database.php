a986f32911350a2c67d0c1419febcb8adc9784b2
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-01-21 03:37:50 +0100

    change class conx from sigltmp to method nprmal

--- database/database.php
+++ database/database.php
@@ -1,34 +1,39 @@
 <?php
 class Database {
-    private static $instance = null;
-    private $pdo;
+    private $host = "localhost";
+    private $username = "root";
+    private $password = "Mahjoub@123";
+    private $database = "taalim";
+    private $connection;
 
-    private function __construct($dsn, $username, $password) {
-        try {
-            $this->pdo = new PDO($dsn, $username, $password);
-            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            // echo 'cnx valid';
-        } catch (PDOException $e) {
-            error_log("Database connection error: " . $e->getMessage());
-            throw new Exception("Database connection failed.");
+    public function __construct() {
+        
+        $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
+        if ($this->connection->connect_error) {
+            die("Connection failed: " . $this->connection->connect_error);
         }
     }
 
-    public static function getInstance($dsn = null, $username = null, $password = null) {
-        if (self::$instance === null) {
-            $dsn = $dsn ?? 'mysql:host=localhost;dbname=platformcours';
-            $username = $username ?? 'root';
-            $password = $password ?? 'Mahjoub@123';
-            self::$instance = new Database($dsn, $username, $password);
-           
-        }
-        return self::$instance;
+    public function prepare($query) {
+        return $this->connection->prepare($query);
     }
 
     public function getConnection() {
-        return $this->pdo;
-
+        return $this->connection;
     }
+
+    public function closeConnection() {
+        $this->connection->close();
+    }
+
+    public function __get($name) {
+        if ($name === 'insert_id') {
+            return $this->connection->insert_id;
+        }
+        return null;
+    }
+
+
 }
 
-// $db = Database::getInstance()->getConnection();
+?>
